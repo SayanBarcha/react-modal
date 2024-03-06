dfc6f26b422f8ecead83efe78deca14690559fe2
SayanBarcha <sayanhunxai124@gmail.com>
2024-03-06 17:24:45 +0500

    modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import StudentAttendanceModal from "./components/StudentAttendanceModal/StudentAttendanceModal.jsx";
+// import axios from "axios";
 
 function App() {
+ const [isOpen, setIsOpen] = useState(false)
+ const openModal = (()=> setIsOpen(true))
+ const closeModal = (()=> setIsOpen(false))
+
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get("API");
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
     <>
       <StudentAttendanceModal/>
+     {/* <button onClick={()=> openModal()}>open modal</button>
+     {isOpen ? (
+      <div>
+        <p>loda</p>
+        <button onClick={()=> closeModal()}>close</button>
+      </div>
+     ): null} */}
+
+      {/* {data.map((item, index) => (
+        <p key={index}>{item?.name}</p>
+      ))} */}
+
     </>
   );
 }
